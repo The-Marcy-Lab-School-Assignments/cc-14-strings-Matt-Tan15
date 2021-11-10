788b53e8b4b8f8f28cfd8200fc38a5fd27dfb7a1
Matthew Tan <tanmatt804@gmail.com>
2021-11-10 15:21:59 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //write your code here
+function stripUpperCase(str) {
+    let newString = "";
+    for (let i = 0; i <= str.length - 1; i++) {
+        if (str.charAt(i) == str.charAt(i).toLowerCase()) {
+            newString += str[i];
+        }
+    }
+    return newString;
+}
+
+console.log(stripUpperCase('Hello!')); // 'ello!')
+console.log(stripUpperCase("SevenEleven")); // 'evenleven'
+console.log(stripUpperCase("Don't play with Me!")); // 'ont play with e!'
